b75b8ffb491c46e7ea6950c4411464f9d2dd76ff
Raul Teixeira <raul.teixeira@camara.leg.br>
2014-10-29 19:44:50 -0200

    Brick now draggeble

--- src/state_laybricks.js
+++ src/state_laybricks.js
@@ -8,7 +8,8 @@ var laybricks = {
 
 	create: function() {
 		this.brickpile = game.add.sprite(game.world.centerX, game.world.centerY, 'brickpile');
-
+		this.brickpile.inputEnabled = true;
+		this.brickpile.input.enableDrag(false, true, false);
 	},
 
 	update: function() {
